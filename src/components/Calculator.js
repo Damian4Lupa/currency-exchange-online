4255bd106c7fa15336bf66c6161acd2eb2f6fb36
Damian Lupa <damian.lupa88@gmail.com>
2019-05-10 22:42:42 +0200

    modified calculator, App.css

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -5,18 +5,64 @@ import exchange from './img/exchange.svg'
 
 class Calculator extends Component {
     state = {
-        rotateButton: false
+        rotateButton: false,
+        valueHave: "100",
+        valueWant: "",
+    }
+
+    data = [
+        { id: "EUR", title: "Euro", symbol: "€" },
+        { id: "BGN", title: "Euro", symbol: "€" },
+        { id: "NZD", title: "Euro", symbol: "€" },
+        { id: "ILS", title: "Euro", symbol: "€" },
+        { id: "RUB", title: "Euro", symbol: "€" },
+        { id: "CAD", title: "Euro", symbol: "€" },
+        { id: "USD", title: "Euro", symbol: "€" },
+        { id: "PHP", title: "Euro", symbol: "€" },
+        { id: "CHF", title: "Euro", symbol: "€" },
+        { id: "ZAR", title: "Euro", symbol: "€" },
+        { id: "AUD", title: "Euro", symbol: "€" },
+        { id: "JPY", title: "Euro", symbol: "€" },
+        { id: "TRY", title: "Euro", symbol: "€" },
+        { id: "HKD", title: "Euro", symbol: "€" },
+        { id: "MYR", title: "Euro", symbol: "€" },
+        { id: "THB", title: "Euro", symbol: "€" },
+        { id: "HRK", title: "Euro", symbol: "€" },
+        { id: "NOK", title: "Euro", symbol: "€" },
+        { id: "IDR", title: "Euro", symbol: "€" },
+        { id: "DKK", title: "Euro", symbol: "€" },
+        { id: "CZK", title: "Euro", symbol: "€" },
+        { id: "HUF", title: "Euro", symbol: "€" },
+        { id: "GBP", title: "Euro", symbol: "€" },
+        { id: "MXN", title: "Euro", symbol: "€" },
+        { id: "KRW", title: "Euro", symbol: "€" },
+        { id: "ISK", title: "Euro", symbol: "€" },
+        { id: "SGD", title: "Euro", symbol: "€" },
+        { id: "BRL", title: "Euro", symbol: "€" },
+        { id: "PLN", title: "Euro", symbol: "€" },
+        { id: "INR", title: "Euro", symbol: "€" },
+        { id: "RON", title: "Euro", symbol: "€" },
+        { id: "CNY", title: "Euro", symbol: "€" },
+        { id: "SEK", title: "Euro", symbol: "€" },
+    ]
+
+
+    handleButtonChange = () => {
+        this.setState({
+            rotateButton: false
+        })
     }
 
     handleButtonRotate = () => {
         this.setState({
-            rotateButton: !this.state.rotateButton
+            rotateButton: true
         })
+        setTimeout(this.handleButtonChange, 1000)
     }
 
     render() {
-
-        const btn_class = this.state.rotateButton ? "rotated" : "exchage"
+        const { valueHave, valueWant } = this.state
+        let btn_class = this.state.rotateButton ? "rotate" : "exchange"
 
         return (
             <div className="alarmInfo">
@@ -32,14 +78,16 @@ class Calculator extends Component {
                                 <option value="3">Three</option>
                             </select>
                             <div className="lg mb-3">
-                                <input type="text" className="form-control form-control-lg" id="colFormLabelLg" placeholder="col-form-label-lg" />
+
+                                <input type="text" className="form-control form-control-lg" id="colFormLabelLg" placeholder={valueHave} value={valueHave} />
+
                             </div>
                         </div>
 
                         <div className="col-2 exchange">
                             <center>
                                 <button type="button" className="btn btn-link btn" onClick={this.handleButtonRotate}>
-                                    <img className={btn_class} src={exchange} alt="" height="82px" />
+                                    <img className={btn_class} src={exchange} alt="exchange" height="82px" />
                                 </button>
                             </center>
                         </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
     return (
         <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
             <h5 className="my-0 mr-md-auto font-weight-normal">Currency exchange online</h5>
-            <nav class="my-2 my-md-0 mr-md-3">
+            <nav className="my-2 my-md-0 mr-md-3">
                 <a className="p-2 text-dark" href="#">About Us</a>
                 <a className="p-2 text-dark" href="#">Security policy</a>
                 <a className="p-2 text-dark" href="#">Help</a>
