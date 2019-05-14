e4d2ae7d07e1c62c7b8e341e3222877fa91b0c45
Damian Lupa <damian.lupa88@gmail.com>
2019-05-14 17:44:35 +0200

    Updates

---
+++ precache-manifest.8f1541a7c7ca9a5056e2a5e1e87e2c52.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52436b6824a275b1cc82faa7124649c9",
+    "url": "/Currency-exchange-online/index.html"
+  },
+  {
+    "revision": "3702a7dcd68f96162bad",
+    "url": "/Currency-exchange-online/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "1aa201bcd48c7e62392d",
+    "url": "/Currency-exchange-online/static/css/main.4aeeff07.chunk.css"
+  },
+  {
+    "revision": "3702a7dcd68f96162bad",
+    "url": "/Currency-exchange-online/static/js/2.9118c41b.chunk.js"
+  },
+  {
+    "revision": "1aa201bcd48c7e62392d",
+    "url": "/Currency-exchange-online/static/js/main.b09d98a0.chunk.js"
+  },
+  {
+    "revision": "d981f73f09cb0458e70c",
+    "url": "/Currency-exchange-online/static/js/runtime~main.64fb8c9e.js"
+  },
+  {
+    "revision": "b9b1de1179d20862af9456526ab45ef0",
+    "url": "/Currency-exchange-online/static/media/cheaper.b9b1de11.jpg"
+  },
+  {
+    "revision": "5eabe0304db34a50afd13726104d0125",
+    "url": "/Currency-exchange-online/static/media/exchange.5eabe030.svg"
+  },
+  {
+    "revision": "85ba980b696c76f0f0ed0a3b0bc4810e",
+    "url": "/Currency-exchange-online/static/media/safety.85ba980b.jpg"
+  },
+  {
+    "revision": "be93f2c85e7cbf1493f361fdea703687",
+    "url": "/Currency-exchange-online/static/media/wallet.be93f2c8.jpg"
+  },
+  {
+    "revision": "5b3ab2d73346757f3697c69706dbc5d3",
+    "url": "/Currency-exchange-online/static/media/working.5b3ab2d7.jpg"
+  }
+]);
