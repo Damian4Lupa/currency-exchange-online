823665d507aabe01e7cc0cabea2ce4773076f847
Damian Lupa <damian.lupa88@gmail.com>
2021-12-02 12:39:27 +0100

    changed class component to functional component

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import "../styles/App.css";
 import "../styles/media.css";
 import Header from "./Header";
@@ -6,17 +6,15 @@ import Calculator from "./Calculator";
 import Offer from "./Offer";
 import Footer from "./Footer";
 
-class App extends Component {
-  render() {
-    return (
-      <>
-        <Header />
-        <Calculator />
-        <Offer />
-        <Footer />
-      </>
-    );
-  }
-}
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Calculator />
+      <Offer />
+      <Footer />
+    </>
+  );
+};
 
 export default App;
