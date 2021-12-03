9b38632448b91a3de2b7eba662cf6cd1fee85a9f
Damian Lupa <damian.lupa88@gmail.com>
2021-12-03 15:49:09 +0100

    code refactoring

--- src/components/SelectIHave.js
+++ src/components/SelectIHave.js
@@ -2,11 +2,9 @@ import React from "react";
 
 const SelectIHave = (props) => {
   return (
-    <>
-      <option value={props.id}>
-        {props.title} ({props.id})
-      </option>
-    </>
+    <option value={props.id}>
+      {props.title} ({props.id})
+    </option>
   );
 };
 
